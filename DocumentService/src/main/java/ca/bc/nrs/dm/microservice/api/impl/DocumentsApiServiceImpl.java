1226242675359ca502e90c9437020ace07b3200f
georgelwalker <gwalker@escapesystems.com>
2017-05-09 15:20:46 -0700

    Documenmt Service updates

--- DocumentService/src/main/java/ca/bc/nrs/dm/microservice/api/impl/DocumentsApiServiceImpl.java
+++ DocumentService/src/main/java/ca/bc/nrs/dm/microservice/api/impl/DocumentsApiServiceImpl.java
@@ -4,7 +4,7 @@ import ca.bc.gov.nrs.dm.rest.client.v1.DocumentManagementService;
 import ca.bc.gov.nrs.dm.rest.client.v1.impl.DocumentManagementServiceImpl;
 import ca.bc.nrs.dm.microservice.api.DocumentsApiService;
 
-import com.google.code.gson;
+//import com.google.code.gson;
 
 import ca.bc.nrs.dm.microservice.model.Document;
 import java.io.InputStream;
@@ -26,9 +26,9 @@ import ca.bc.gov.nrs.dm.rest.client.v1.impl.DocumentManagementServiceImpl;
 import ca.bc.gov.nrs.dm.service.v1.DMServiceResponse;
 import ca.bc.gov.nrs.dm.service.v1.FileService;
 import ca.bc.gov.nrs.dm.service.v1.FolderService;
-import ca.bc.gov.webade.oauth2.rest.test.client.AuthorizationCodeService;
-import ca.bc.gov.webade.oauth2.rest.test.client.impl.AuthorizationCodeServiceImpl;
-import ca.bc.gov.webade.oauth2.rest.test.resource.AuthorizationCode;
+//import ca.bc.gov.webade.oauth2.rest.test.client.AuthorizationCodeService;
+//import ca.bc.gov.webade.oauth2.rest.test.client.impl.AuthorizationCodeServiceImpl;
+//import ca.bc.gov.webade.oauth2.rest.test.resource.AuthorizationCode;
 import ca.bc.gov.webade.oauth2.rest.v1.token.client.Oauth2ClientException;
 import ca.bc.gov.webade.oauth2.rest.v1.token.client.TokenService;
 import ca.bc.gov.webade.oauth2.rest.v1.token.client.impl.TokenServiceImpl;
@@ -56,6 +56,12 @@ public class DocumentsApiServiceImpl implements DocumentsApiService {
       private static String siteMinderUserId = "NOT\\USED";
       private static String documentManagementTopLevelRestURL;
       private static String oauth2ResourceName;   
+      
+      private OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails = null;
+      
+      public void setOAuth2ProtectedResourceDetails(OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails) {
+        this.oAuth2ProtectedResourceDetails = oAuth2ProtectedResourceDetails;
+      }
       
       public DocumentsApiServiceImpl()
       {
@@ -87,28 +93,28 @@ public class DocumentsApiServiceImpl implements DocumentsApiService {
 
 		String smartFormsTestClient = "";
 		String smartFormsTestSecret = "";
-	
+	        /*
 		AuthorizationCodeService authorizationCodeService;
-          authorizationCodeService = new AuthorizationCodeServiceImpl(smartFormsTestClient, authorizeUrl);
+                authorizationCodeService = new AuthorizationCodeServiceImpl(smartFormsTestClient, authorizeUrl);
 		TokenService tokenService = new TokenServiceImpl(smartFormsTestClient, smartFormsTestSecret, null, tokenUrl);
 		
 		// Getting the application code
 		AuthorizationCode authorizationCode = authorizationCodeService.getAuthorizationCode(SCOPES, ownerOrganizationId, redirectUri, 
 		AuthorizationCodeService.SITEMINDER_USER_TYPE_BUSINESS, siteMinderUserId, siteMinderUserGuid, null, null, "code");
+*/
 		//then we use this code to get the token 
-		AccessToken token = tokenService.getToken(USER_SCOPES, authorizationCode.getCode(), redirectUri);
-		
+		//AccessToken token = tokenService.getToken(USER_SCOPES, authorizationCode.getCode(), redirectUri);
+		AccessToken token = new AccessToken();
 		DocumentManagementService clientService = createDMSClientService(token);
 		
 		return clientService;
 	}
       
+        
       
 	private DocumentManagementService createDMSClientService( AccessToken token )
-	{
-		OAuth2ProtectedResourceDetails resource =  new OAuth2ProtectedResourceDetails();
-                
-		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource);
+	{                
+		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(oAuth2ProtectedResourceDetails);
 		OAuth2ClientContext context = restTemplate.getOAuth2ClientContext();
 		context.setAccessToken(new DefaultOAuth2AccessToken(token.getAccessToken()));
 
