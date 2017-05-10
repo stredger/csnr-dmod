b1242ad4292c6eb95fadda86429624d0e340449c
georgelwalker <gwalker@escapesystems.com>
2017-05-09 18:34:51 -0700

    Update production configuration

--- ExampleMeanApp/config/env/production.js
+++ ExampleMeanApp/config/env/production.js
@@ -2,37 +2,26 @@
 
 module.exports = {
   secure: {
-    ssl: true,
-    privateKey: './config/sslcerts/key.pem',
-    certificate: './config/sslcerts/cert.pem'
+    ssl: false
   },
-  port: process.env.PORT || 8443,
+  port: process.env.PORT || 3000,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/' + (process.env.MONGODB_DATABASE || 'esm'),
+    acluri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/esm-acl',
     options: {
-      user: '',
-      pass: ''
+      user: process.env.MONGODB_USER || '',
+      pass: process.env.MONGODB_PASSWORD || ''
     },
-    // Enable mongoose debug mode
+    // Enable mongoose debug module
     debug: process.env.MONGODB_DEBUG || false
   },
   log: {
-    // logging with Morgan - https://github.com/expressjs/morgan
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: process.env.LOG_FORMAT || 'combined',
+    format: 'combined',
+    // Stream defaults to process.stdout
+    // Uncomment to enable logging to a log on the file system
     options: {
-      // Stream defaults to process.stdout
-      // Uncomment/comment to toggle the logging to a log on the file system
-      stream: {
-        directoryPath: process.env.LOG_DIR_PATH || process.cwd(),
-        fileName: process.env.LOG_FILE || 'access.log',
-        rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-          active: process.env.LOG_ROTATING_ACTIVE === 'true' ? true : false, // activate to use rotating logs 
-          fileName: process.env.LOG_ROTATING_FILE || 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
-          frequency: process.env.LOG_ROTATING_FREQUENCY || 'daily',
-          verbose: process.env.LOG_ROTATING_VERBOSE === 'true' ? true : false
-        }
-      }
+      stream: 'access.log'
     }
   },
   facebook: {
@@ -66,38 +55,22 @@ module.exports = {
     callbackURL: '/api/auth/paypal/callback',
     sandbox: false
   },
+  token: {
+    tokenQuery: process.env.TOKEN_QUERY || undefined,
+    tokenParams: process.env.TOKEN_PARAMS || undefined,
+    tokenField: process.env.TOKEN_FIELD || undefined,
+    tokenHeader: process.env.TOKEN_HEADER || 'smgov_userguid',
+    failedOnMissing: process.env.TOKEN_ON_MISSING || false
+  },
   mailer: {
-    from: process.env.MAILER_FROM || 'MAILER_FROM',
+    from: process.env.MAILER_FROM || '"BC Environmental Assessment Office" <noreply@projects.eao.gov.bc.ca>',
     options: {
-      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
-      auth: {
-        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+      name: 'projects.eao.gov.bc.ca',
+      host: 'apps.smtp.gov.bc.ca',
+      tls: {
+        rejectUnauthorized: false
       }
     }
   },
-  seedDB: {
-    seed: process.env.MONGO_SEED === 'true' ? true : false,
-    options: {
-      logResults: process.env.MONGO_SEED_LOG_RESULTS === 'false' ? false : true,
-      seedUser: {
-        username: process.env.MONGO_SEED_USER_USERNAME || 'user',
-        provider: 'local',
-        email: process.env.MONGO_SEED_USER_EMAIL || 'user@localhost.com',
-        firstName: 'User',
-        lastName: 'Local',
-        displayName: 'User Local',
-        roles: ['user']
-      },
-      seedAdmin: {
-        username: process.env.MONGO_SEED_ADMIN_USERNAME || 'admin',
-        provider: 'local',
-        email: process.env.MONGO_SEED_ADMIN_EMAIL || 'admin@localhost.com',
-        firstName: 'Admin',
-        lastName: 'Local',
-        displayName: 'Admin Local',
-        roles: ['user', 'admin']
-      }
-    }
-  }
+  seedDB: process.env.MONGO_SEED || true
 };
